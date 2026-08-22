09b4dbfd50d6f73af255bb96086b45c4e24d5359
Gabriel William Teñoso <gabrielwilliam0118@gmail.com>
2026-08-22 16:26:47 +0800

    day 3 and 4 feat: api connected css

--- React Movie Tutorial/frontend/src/pages/Home.jsx
+++ React Movie Tutorial/frontend/src/pages/Home.jsx
@@ -1,13 +1,40 @@
 import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
+import "../css/Home.css";
+import { searchMovies, getPopularMovies } from "../services/api";
 
 function Home() {
-  const movies = [
-    { id: 1, title: "John Wick", release_date: "2020" },
-    { id: 2, title: "Terminator", release_date: "1999" },
-    { id: 3, title: "The Matrix", release_date: "1998" },
-  ];
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearch = () => {};
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPopularMovies();
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) return;
+    if (loading) return;
+
+    setLoading(true);
+
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
@@ -16,17 +43,28 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         ></input>
         <button type="submit" className="search-button">
           Search
         </button>
       </form>
 
-      <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
-      </div>
+      {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map(
+            (movie) =>
+              movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id} />
+              ),
+          )}
+        </div>
+      )}
     </div>
   );
 }
---
+++ React Movie Tutorial/frontend/src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "f30c302ce90e0b7708585bb974b67079";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
