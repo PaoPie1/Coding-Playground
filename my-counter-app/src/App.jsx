88022b4821ca899466bf9b38a3406f3712fa162e
Gabriel William Teñoso <gabrielwilliam0118@gmail.com>
2026-08-26 10:16:35 +0800

    feat: button sound effect pop

--- my-counter-app/src/App.jsx
+++ my-counter-app/src/App.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 import "./App.css";
 
+import sound from "./assets/pop.wav";
+
 function App() {
   const [count, setCount] = useState(0);
 
   const [actionType, setActionType] = useState(null);
 
+  const playSound = new Audio(sound);
+
   const handleAdd = () => {
     setCount(count + 1);
     setActionType("add");
+    playSound.play();
   };
 
   const handleMinus = () => {
     setCount(count - 1);
     setActionType("minus");
+    playSound.play();
   };
 
   const handleReset = () => {
     setCount(0);
     setActionType(null);
+    playSound.play();
   };
 
   return (
