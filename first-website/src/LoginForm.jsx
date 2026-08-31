ad1bb3ea89d4b65c8f91484c1a180499c253b7a3
Gabriel William Teñoso <gabrielwilliam0118@gmail.com>
2026-08-31 20:02:26 +0800

    feat: login component

--- first-website/src/LoginForm.jsx
+++ first-website/src/LoginForm.jsx
@@ -1,35 +1,27 @@
 import "./LoginForm.css";
 
-function LoginForm({ username, setUsername, password, setPassword, onLogin }) {
+function LoginForm() {
   return (
     <div className="login-container">
-      <form onSubmit={onLogin} className="login-form">
-        <h2>Login</h2>
+      <form className="login-form">
+        <h2>Account Login</h2>
 
         <div className="input-group">
           <label>Username</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter Username..."
-            required
-          />
+          <input type="text" placeholder="Enter username..." required></input>
         </div>
 
         <div className="input-group">
           <label>Password</label>
           <input
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
             placeholder="Enter password..."
             required
           ></input>
         </div>
 
-        <button type="submit" className="submit-btn">
-          Login
+        <button className="submit-btn" type="submit">
+          LOGIN
         </button>
       </form>
     </div>
@@ -37,3 +29,43 @@ function LoginForm({ username, setUsername, password, setPassword, onLogin }) {
 }
 
 export default LoginForm;
+
+// import "./LoginForm.css";
+
+// function LoginForm({ username, setUsername, password, setPassword, onLogin }) {
+//   return (
+//     <div className="login-container">
+//       <form onSubmit={onLogin} className="login-form">
+//         <h2>ACCOUNT</h2>
+
+//         <div className="input-group">
+//           <label>Username</label>
+//           <input
+//             type="text"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//             placeholder="Enter Username..."
+//             required
+//           />
+//         </div>
+
+//         <div className="input-group">
+//           <label>Password</label>
+//           <input
+//             type="password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             placeholder="Enter password..."
+//             required
+//           ></input>
+//         </div>
+
+//         <button type="submit" className="submit-btn">
+//           Login
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default LoginForm;
